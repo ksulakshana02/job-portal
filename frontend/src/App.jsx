5cfc74579efdf71e3b99607307850a21ca8edfe4
ksulakshana02 <sulakshanakaveesha@gmail.com>
2025-02-10 04:58:31 +0530

    updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,11 @@ import Footer from "./components/Footer.jsx";
 import Navbar from "./components/Navbar.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import Jobs from "./pages/Jobs.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import PostApplication from "./pages/PostApplication.jsx";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
+import {ToastContainer} from "react-toastify";
 
 const App = () => {
     return (
@@ -12,10 +17,15 @@ const App = () => {
                 <Navbar/>
                 <Routes>
                     <Route path="/" element={<Home/>}/>
-                    <Route path="/jobs" element={<Jobs />}/>
+                    <Route path="/jobs" element={<Jobs/>}/>
+                    <Route path="/dashboard" element={<Dashboard/>}/>
+                    <Route path="/post/application/:jobId" element={<PostApplication/>}/>
+                    <Route path="/register" element={<Register/>}/>
+                    <Route path="/login" element={<Login/>}/>
                     <Route path="*" element={<NotFound/>}/>
                 </Routes>
                 <Footer/>
+                <ToastContainer position="top-right" theme="dark"/>
             </Router>
         </>
     )
